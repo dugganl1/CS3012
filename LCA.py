31f9ca565602ba1eba1bd2dbc928f9b0aa1f755a
dugganl1 <dugganl1@tcd.ie>
2018-09-29 16:26:24 +0100

    LCA Update
    Update to main file

--- LCA.py
+++ LCA.py
@@ -1,5 +1,29 @@
 print('Hello World')
 
+# Task: Given Binary Tree and two inputs x and y, write a program to find their LOWEST COMMON ANCESTOR
 
-def addOne(x):
-    return x + 1
+
+class Node:
+    # Constructor
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+
+class Solve(object):
+    def lowestCommonAncestor(self, root, x, y):
+
+        if not root or y == root or x == root:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, x, y)
+        right = self.lowestCommonAncestor(root.right, x, y)
+
+        if left and right:
+            return root
+        if left:
+            return left
+        elif right:
+            return right
+        return None
