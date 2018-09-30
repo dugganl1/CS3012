1995884988ddc967a31d5928f8ce4ca363ba1fd0
dugganl1 <dugganl1@tcd.ie>
2018-10-01 00:28:49 +0100

    Trying to get testing going

--- test_lca_unittest.py
+++ test_lca_unittest.py
@@ -1,13 +1,21 @@
 import unittest
-from LCA import addOne
+from LCA import Solve, Node
+
+root = Node(20)
+root.left = Node(8)
+root.right = Node(22)
+root.left.left = Node(4)
+root.left.right = Node(12)
+root.left.right.left = Node(10)
+root.left.right.right = Node(14)
 
 
 class testLCA(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_addOne(self):
-        self.assertEqual(addOne(3), 4)
+    def test_lowestCommonAncestor(self):
+        self.assertEqual(4, 4)
 
 
 if __name__ == '__main__':
