87fdfe2c9b32ffcece6692759ca6a0238c466e33
dugganl1 <dugganl1@tcd.ie>
2018-10-07 15:05:14 +0100

    Further Work
    Had more time today than during the week, so have an opportunity to work on code further and get to grips with Python

--- LCA.py
+++ LCA.py
@@ -1,8 +1,68 @@
-print('Hello World')
-
 # Task: Given Binary Tree and two inputs x and y, write a program to find their LOWEST COMMON ANCESTOR
 
 
+class Tree:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def insert_left(self, x):
+        if self.left is None:
+            self.left = Tree(x)
+        else:
+            new_node = Tree(x)
+            new_node.left = self.left
+            self.left = new_node
+
+    def insert_right(self, x):
+        if self.right is None:
+            self.right = Tree(x)
+        else:
+            new_node = Tree(x)
+            new_node.right = self.right
+            self.right = new_node
+
+    def lowestCommonAncestor(self, root, p, q):
+        source = self
+
+        if p is None or q is None:
+            return self.ancestor(source.p) or self.ancestor(source.q)
+
+        if self.ancestor(source, p) is None:
+            print('One or both nodes are not part of the tree')
+
+        if self.ancestor(source, q) is None:
+            print('One or both nodes are not part of the tree')
+
+        if(p == q):
+            print('Cant compare the same node')
+
+        if root in [None, p, q]:
+            return root
+
+        left, right = (self.lowestCommonAncestor(kid, p, q) for kid in (root.left, root.right))
+
+        if left and right:
+            return root
+        else:
+            return left or right
+
+    def ancestor(self, root, node):
+        if root is not None:
+            if root == node:
+                return node
+            if root.left is not None and root.left == node:
+                return root
+            if root.right is not None and root.right == node:
+                return root
+            elif root:
+                return self.ancestor(root.left, node) or self.ancestor(root.right, node)
+            else:
+                return None
+
+
+'''
 class Node(object):
     # Constructor
     def __init__(self, x):
@@ -29,3 +89,4 @@ class Solve(object):
             return right
         else:
             return left
+'''
--- test_lca_unittest.py
+++ test_lca_unittest.py
@@ -1,21 +1,44 @@
 import unittest
-from LCA import Solve, Node
+import LCA
 
-root = Node(8)
-root.left = Node(12)
-root.right = Node(21)
-root.left.left = Node(14)
-root.left.right = Node(7)
-root.left.right.left = Node(9)
-root.left.right.right = Node(3)
+tree = LCA.Tree(1)
+root = tree
+root.insert_left(2)
+root.insert_right(3)
+root.left.insert_left(4)
+root.left.insert_right(5)
 
 
 class testLCA(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_same_node(self):
+        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left, tree.left), tree.left)
+
     def test_lowestCommonAncestor(self):
-        self.assertEqual(Solve().lowestCommonAncestor(root, root.left.right, root.right).val, 8)
+        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left, tree.right), root)
+
+    def test_p_is_root(self):
+        self.assertEqual(tree.lowestCommonAncestor(tree, root, tree.left), root)
+
+    def test_different_levels(self):
+        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left.left, tree.right), root)
+
+    def test_ancestor_not_root(self):
+        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left.left, tree.left.right), tree.left)
+
+        # Test no common ancestor
+        # Test that it returns None if the tree is empty
+        # Test that it returns itself if there is only one element in the tree
+        # Test LCA for nodes that aren't on the same tier
+        # Test LCA between non-existent nodes
+        # Test LCA between one existing node and one non-existent node
+        # Test LCA between the root and another node
+        # Test LCA when root is null
+        # Test LCA of a node is itself
+
+    print('Sucess!')
 
 
 if __name__ == '__main__':
