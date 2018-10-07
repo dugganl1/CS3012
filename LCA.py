5d829baf2d6723abf62c3ebe1aeaca051f0cd145
dugganl1 <dugganl1@tcd.ie>
2018-10-07 18:09:19 +0100

    Weekend Work
    After exploring a few options, decided to opt for simple GeeksForGeeks implementation for LCA.py. Switched focus to Test Code. Added numerous tests for various potential cases. Ran Coverage tests. Exported coverage report to html (100% coverage).

--- LCA.py
+++ LCA.py
@@ -1,92 +1,45 @@
 # Task: Given Binary Tree and two inputs x and y, write a program to find their LOWEST COMMON ANCESTOR
-
-
-class Tree:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-    def insert_left(self, x):
-        if self.left is None:
-            self.left = Tree(x)
-        else:
-            new_node = Tree(x)
-            new_node.left = self.left
-            self.left = new_node
-
-    def insert_right(self, x):
-        if self.right is None:
-            self.right = Tree(x)
-        else:
-            new_node = Tree(x)
-            new_node.right = self.right
-            self.right = new_node
-
-    def lowestCommonAncestor(self, root, p, q):
-        source = self
-
-        if p is None or q is None:
-            return self.ancestor(source.p) or self.ancestor(source.q)
-
-        if self.ancestor(source, p) is None:
-            print('One or both nodes are not part of the tree')
-
-        if self.ancestor(source, q) is None:
-            print('One or both nodes are not part of the tree')
-
-        if(p == q):
-            print('Cant compare the same node')
-
-        if root in [None, p, q]:
-            return root
-
-        left, right = (self.lowestCommonAncestor(kid, p, q) for kid in (root.left, root.right))
-
-        if left and right:
-            return root
-        else:
-            return left or right
-
-    def ancestor(self, root, node):
-        if root is not None:
-            if root == node:
-                return node
-            if root.left is not None and root.left == node:
-                return root
-            if root.right is not None and root.right == node:
-                return root
-            elif root:
-                return self.ancestor(root.left, node) or self.ancestor(root.right, node)
-            else:
-                return None
-
-
-'''
+#GeeksForGeeks implementation
 class Node(object):
     # Constructor
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
 
 
-class Solve(object):
-    def lowestCommonAncestor(self, root, p, q):
+def findPath(root, path, k):
+    if root is None:
+        return False
 
-        if root is None:
-            return None
+    # Store node in path vector, Removed if not in path from root to k
+    path.append(root.key)
 
-        if root == p or root == q:
-            return root
+    # see if k is the same as the root's key
+    if root.key == k:
+        return True
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+    # Check if in left or right subtree
+    if(root.left is not None and findPath(root.left, path, k)) or (root.right is not None and findPath(root.right, path, k)):
+        return True
 
-        if left is not None and right is not None:
-            return root
-        if left is None:
-            return right
-        else:
-            return left
-'''
+# If it gets this far, it means its not present so lets remove the root from the path and return False
+    path.pop()
+    return False
+
+
+def findLowestCommonAncestor(root, p, q):
+    # store paths
+    path1 = []
+    path2 = []
+
+    if not findPath(root, path1, p) or not findPath(root, path2, q):
+        return -1
+
+    # Compare paths to get first different value, and return the node index before this
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
+            break
+        i += 1
+    return path1[i - 1]
--- test_lca_unittest.py
+++ test_lca_unittest.py
@@ -1,44 +1,91 @@
 import unittest
-import LCA
+from LCA import *
 
-tree = LCA.Tree(1)
-root = tree
-root.insert_left(2)
-root.insert_right(3)
-root.left.insert_left(4)
-root.left.insert_right(5)
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+'''
+        1
+      /   \
+     2     3
+    /\     /\
+   4  5   6  7
+'''
 
 
 class testLCA(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_same_node(self):
-        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left, tree.left), tree.left)
+    def test_normal_cases(self):
+        # Same-level
+        self.assertEqual(findLowestCommonAncestor(root, 2, 3), 1)
+        self.assertEqual(findLowestCommonAncestor(root, 4, 6), 1)
+        # Different-Level
+        self.assertEqual(findLowestCommonAncestor(root, 2, 7), 1)
+        self.assertEqual(findLowestCommonAncestor(root, 4, 3), 1)
 
-    def test_lowestCommonAncestor(self):
-        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left, tree.right), root)
+    def test_lca_not_root(self):
+        self.assertEqual(findLowestCommonAncestor(root, 6, 7), 3)
+        self.assertEqual(findLowestCommonAncestor(root, 4, 5), 2)
 
-    def test_p_is_root(self):
-        self.assertEqual(tree.lowestCommonAncestor(tree, root, tree.left), root)
+    def test_root_as_input(self):
+        self.assertEqual(findLowestCommonAncestor(root, 1, 4), 1)
 
-    def test_different_levels(self):
-        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left.left, tree.right), root)
+    def test_lca_same_node(self):
+        self.assertEqual(findLowestCommonAncestor(root, 1, 1), 1)
+        self.assertEqual(findLowestCommonAncestor(root, 7, 7), 7)
 
-    def test_ancestor_not_root(self):
-        self.assertEqual(tree.lowestCommonAncestor(tree, tree.left.left, tree.left.right), tree.left)
+    def test_only_one_exists(self):
+        self.assertEqual(findLowestCommonAncestor(root, 4, 10), -1)
+        self.assertEqual(findLowestCommonAncestor(root, 21, 7), -1)
 
-        # Test no common ancestor
-        # Test that it returns None if the tree is empty
-        # Test that it returns itself if there is only one element in the tree
-        # Test LCA for nodes that aren't on the same tier
-        # Test LCA between non-existent nodes
-        # Test LCA between one existing node and one non-existent node
-        # Test LCA between the root and another node
-        # Test LCA when root is null
-        # Test LCA of a node is itself
+    def test_neither_exist(self):
+        self.assertEqual(findLowestCommonAncestor(root, 10, 8), -1)
 
-    print('Sucess!')
+    def test_null_tree(self):
+        root = None
+        self.assertEqual(findLowestCommonAncestor(root, 2, 3), -1)
+        path = []
+        self.assertFalse(findPath(root, path, 8))
+        self.assertListEqual(path, [])
+
+    def test_only_one_node(self):
+        root = Node(10)
+        self.assertEqual(findLowestCommonAncestor(root, 10, 10), 10)
+
+    def test_path_finder(self):
+        path = []
+        # Root to itself
+        self.assertTrue(findPath(root, path, 1))
+        self.assertListEqual(path, [1])
+
+        path = []
+        # Right Side
+        self.assertTrue(findPath(root, path, 7))
+        self.assertListEqual(path, [1, 3, 7])
+
+        path = []
+        # Left Side
+        self.assertTrue(findPath(root, path, 2))
+        self.assertListEqual(path, [1, 2])
+
+        path = []
+        # No Path
+        self.assertFalse(findPath(root, path, 12))
+        self.assertListEqual(path, [])
+
+        # Path not from root
+        path = []
+        self.assertTrue(findPath(root.right, path, 7))
+        self.assertListEqual(path, [3, 7])
+
+    print('Sucess.')
 
 
 if __name__ == '__main__':
