0a13b2a99808a154aecb29183f4c2aef4dc99928
dugganl1 <dugganl1@tcd.ie>
2018-10-14 17:19:32 +0100

    Setting Up DAG File

---
+++ DAG.py
@@ -0,0 +1,31 @@
+# Lowest Common Ancestor Problem with DAG implementation
+class DAG(object):
+
+    class Node:
+        def __init__(self, key):
+            self.key = key
+            self.links = []
+
+        def pointsTo(self, node):
+            self.links.append(node)
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, node, graph=None):
+        if not graph:
+            graph = self.graph
+
+        if node in graph:
+            return False
+
+        graph[node] = []
+
+    def add_edge(self, node_one, node_two, graph=None):
+        if not graph:
+            graph = self.graph
+
+        if node_one in graph and node_two in graph:
+            graph[node_one].append(node_two)
+        else:
+            raise ValueError("Both keys must exist")
